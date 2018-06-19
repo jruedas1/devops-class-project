1cb68ccbbc88e1e7d02b707d06b8b552267229f4
averysmith65 <avery@ngparts.com>
2018-06-19 15:01:36 -0500

    added db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -36,19 +36,6 @@ ActiveRecord::Schema.define(version: 2018_06_18_221645) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "students", force: :cascade do |t|
-    t.string "title"
-    t.string "location"
-    t.string "skills"
-    t.string "summary"
-    t.string "github"
-    t.string "linkedin"
-    t.string "school"
-    t.string "photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
