604045915ef29328cb4d79abe5dd128d8dd7c286
leslie <l.henley10@yahoo.com>
2018-06-19 16:04:27 -0500

    Added user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_18_225211) do
+ActiveRecord::Schema.define(version: 2018_06_19_205911) do
 
   create_table "recruiters", force: :cascade do |t|
     t.string "skills"
@@ -35,19 +35,7 @@ ActiveRecord::Schema.define(version: 2018_06_18_225211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "jobtypes"
-  end
-
-  create_table "students", force: :cascade do |t|
-    t.string "title"
-    t.string "location"
-    t.string "skills"
-    t.string "summary"
-    t.string "github"
-    t.string "linkedin"
-    t.string "school"
-    t.string "photo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
