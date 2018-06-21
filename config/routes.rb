c646d9b36c7c4a63433b6b4a324bf76417a7330b
Javier Ruedas <jruedas@twc.com>
2018-06-21 14:43:12 -0500

    recruiter form works and displays to view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'recruiter/new'
+  get 'recruiter/edit'
+  get 'recruiters' => 'recruiter#index'
   devise_for :users
   root 'welcome#index'
   get 'welcome/index'
+  post 'recruiters' => 'recruiter#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
