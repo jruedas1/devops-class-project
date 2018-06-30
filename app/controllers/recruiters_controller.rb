d9ce7d3c32fbe7c14c04d49e1a43830b4107a8f0
Javier Ruedas <jruedas@twc.com>
2018-06-30 12:26:59 -0500

    show, index, and new working with new recruiters controller

--- app/controllers/recruiters_controller.rb
+++ app/controllers/recruiters_controller.rb
@@ -7,8 +7,21 @@ class RecruitersController < ApplicationController
   end
 
   def show
+  	@recruiter = Recruiter.find(params[:id])
   end
 
   def new
+  	@recruiter = Recruiter.new
+  end
+
+  def create
+    @recruiter = Recruiter.create(recruiter_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def recruiter_params
+     params.require(:recruiter).permit(:name, :email, :company, :website, :linkedin, :bio, :user_id)
   end
 end
--- spec/controllers/recruiter_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe RecruiterController, type: :controller do
-
-
-end
