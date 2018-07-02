23484f3d85e9394f739db4b8f3b7957222d4d908
joshy91 <joshua.davidsonsga@gmail.com>
2018-07-02 16:38:56 -0500

    Structured and added content for the index new and show page for the Students controller

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,34 @@
+class StudentsController < ApplicationController
+def index
+  	@students = Student.all
+  end
+
+  def edit
+     @student = Student.find(params[:id])
+  end
+
+  def show
+  	@student = Student.find(params[:id])
+  end
+
+  def new
+  	@student = Student.new
+  end
+
+  def create
+    @student = Student.create(student_params)
+    redirect_to students_path
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(params[:id])
+  end
+
+  private
+
+  def student_params
+     params.require(:student).permit(:title, :location, :skills, :summary, :github, :linkedin, :school, :jobtypes, :photo, :user_id)
+  end
+end
